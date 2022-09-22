1ca532c20ef0134d3397be7dc38ebd40054801f0
D-hash-code <73314268+D-hash-code@users.noreply.github.com>
2022-09-22 23:27:32 +0100

    update home page with new auth process

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,7 +1,6 @@
 import type {NextPage} from 'next'
 import {SetStateAction, useState} from "react";
 import Container from "../Components/Container";
-import {useAuthContext} from "../context/auth";
 import {TextField} from "@mui/material";
 import Button from '@mui/material/Button';
 import SendIcon from '@mui/icons-material/Send';
@@ -11,6 +10,7 @@ import LoadingButton from '@mui/lab/LoadingButton';
 import cn from 'classnames';
 import axios from 'axios';
 import {SearchResultV2} from "./api/searchV2";
+import {withPageAuthRequired} from "@auth0/nextjs-auth0";
 
 type Response = {
     data: {
@@ -19,10 +19,10 @@ type Response = {
     };
     status: number;
 }
+export const getServerSideProps = withPageAuthRequired();
 
-
-const Home: NextPage = () => {
-    const {auth, setAuth, setApproved, approved, uid, setUid} = useAuthContext();
+//@ts-ignore
+const Home: NextPage = ({user}) => {
     const [query, setQuery] = useState("");
     const [searchResults, setSearchResults] = useState<SearchResultV2[] | null>(null);
     const [loading, setLoading] = useState(false);
@@ -33,7 +33,7 @@ const Home: NextPage = () => {
             query
         }, {
             headers: {
-                'Authorization': `Bearer ${auth ? uid : ''}`
+                'Authorization': `Bearer `
             }
         }).then((res: Response) => {
             setLoading(false);
@@ -55,9 +55,8 @@ const Home: NextPage = () => {
             <div className="flex w-full mt-4 gap-2">
                 <TextField
                     id="search"
-                    label={auth ? "Enter Search Query" : "Login to Search"}
+                    label={"Enter Search Query"}
                     variant="outlined"
-                    disabled={!auth || !approved}
                     className="w-full"
                     onChange={(e) => {
                         setQuery(e.target.value)
@@ -66,7 +65,7 @@ const Home: NextPage = () => {
                 <div>
                     <LoadingButton
                         className={cn(
-                            (!auth || !approved || !query) ? 'opacity-50 cursor-not-allowed' : '',
+                            (!query) ? 'opacity-50 cursor-not-allowed' : '',
                             "h-full bg-green-mid text-white"
                         )}
                         size="small"
@@ -75,30 +74,11 @@ const Home: NextPage = () => {
                         loading={loading}
                         loadingPosition="end"
                         variant="contained"
-                        disabled={!auth || !approved || !query}
+                        disabled={!query}
                     >
                         Search
                     </LoadingButton>
                 </div>
-            </div>
-            <div>
-                {!auth && !approved && (
-                    <div className="flex flex-col gap-2 mt-4">
-                        <p className="text-red-500">
-                            In order to access the features of this search engine you must:
-                        </p>
-                        <ul className="list-disc list-inside text-red-500">
-                            <li>Log in with your Google account</li>
-                            <li>Wait for approval from the admin</li>
-                        </ul>
-                        <p>
-                            Please Note, access is limited at the moment.
-                        </p>
-                        <p>
-                            We will expand access once we come out of beta.
-                        </p>
-                    </div>
-                )}
             </div>
             <div className="flex flex-col gap-12 mt-4">
                 {(searchResults) && (

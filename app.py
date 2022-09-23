1d08d8266619f75442dde0ce80f5215e73819465
D-hash-code <73314268+D-hash-code@users.noreply.github.com>
2022-09-23 18:53:08 +0100

    change port number

--- app.py
+++ app.py
@@ -405,5 +405,5 @@ def get_document():
 
 if __name__ == '__main__':
     if 'liveconsole' not in gethostname():
-        app.run(debug=True, port=5001)
+        app.run(debug=True, port=5000)
         reset_db()
